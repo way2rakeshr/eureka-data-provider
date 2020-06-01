02923a617672ce53e1a10a7399bc7af5d26c9e27
way2rakeshr <way2rakeshr@gmail.com>
2020-06-01 15:56:59 +0530

    intial commit

---
+++ src/test/java/com/ocp/eureka/provider/EurekaDataProviderTest.java
@@ -0,0 +1,16 @@
+package com.ocp.eureka.provider;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EurekaDataProviderTest {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
